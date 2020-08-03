f8d7519071108f189e46698ff2c491b84da1228a
Ruben Gines <43281288+Ruben243@users.noreply.github.com>
2020-08-03 22:21:52 +0200

    Update email.js

--- proyecto node/config/email.js
+++ proyecto node/config/email.js
@@ -1,6 +1,6 @@
 module.exports = {
-    user: '55da51b7eedb00',
-    pass: 'b970493fce20c4',
-    host: 'smtp.mailtrap.io',
-    port: '2525'
+    user: '',
+    pass: '',
+    host: '',
+    port: ''
 }
